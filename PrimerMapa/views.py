514c89ac5cc43f62d149c5f9700b4673ba083703
junior <freesoftandskate@gmail.com>
2017-08-13 14:17:55 -0500

    Carga el mapa con google maps

--- PrimerMapa/views.py
+++ PrimerMapa/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
+from .models import Waypoint
 
 
 # Create your views here.
 
 def index(response):
+    waypoints = Waypoint.objects.order_by('name')
+    """return render(response, 'PrimerMapa/index.html', {
+        'waypoints': waypoints,
+        'content': render_to_string('PrimerMapa/waypoints.html', {
+            'waypoints': waypoints
+        })
+    })"""
     return render(response, 'PrimerMapa/index.html', {})
